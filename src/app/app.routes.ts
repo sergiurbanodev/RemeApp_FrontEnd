fd9cd934ed649d6ae0c0dd1c694a9ddca3d6e351
Sergi Urbano Alabró <sergi.urbano18@gmail.com>
2026-05-20 09:26:47 +0200

    feat: added first routes and redirect config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from '@layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'demon-list',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'demon-list',
+        loadComponent: () => import('@pages/demon-list/demon-list').then((m) => m.DemonList),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'demon-list',
+  },
+];
