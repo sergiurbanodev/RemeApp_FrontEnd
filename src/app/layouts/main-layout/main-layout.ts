0d97c4ceb8c90b2fc955cd18acfe92f3a32a1af5
Sergi Urbano Alabró <sergi.urbano18@gmail.com>
2026-05-20 13:51:35 +0200

    fix: separated header, main and footer correctly

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StarBackground } from '@layouts/components/star-background/star-background';
-import { NavBar } from '@shared/nav-bar/nav-bar';
-import { ProfileButton } from '@shared/profile-button/profile-button';
+import { NavBar } from '@layouts/components/nav-bar/nav-bar';
+import { ProfileButton } from '@layouts/components/profile-button/profile-button';
+import { Footer } from '@layouts/components/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavBar, RouterOutlet, StarBackground, ProfileButton],
+  imports: [NavBar, RouterOutlet, StarBackground, ProfileButton, Footer],
   templateUrl: './main-layout.html',
 })
 export class MainLayout {}
